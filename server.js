e9c3ef079e92251f2a2d7ef309826a491508c07a
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-03-07 22:53:48 -0300

    adiciona chat online

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (request, response) => {
+    response.render('index.html');
+});
+
+let messages = [];
+
+io.on('connection', socket => {
+    console.log(`socket conectado: ${socket.id}`);
+
+    socket.emit('previousMessages', messages);
+
+    socket.on('sendMessage', message => {
+        messages.push(message);
+        socket.broadcast.emit('receivedMessage', message);
+    })
+})
+
+server.listen(3000);
